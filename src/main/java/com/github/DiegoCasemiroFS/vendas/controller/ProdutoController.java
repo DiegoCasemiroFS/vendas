c8cc461eae217d485c7492f4f7e5032f8583504b
dcfs1 <dcfs1998@gmail.com>
2023-05-15 20:50:51 -0300

    incluindo classe service e controller para produto

---
+++ src/main/java/com/github/DiegoCasemiroFS/vendas/controller/ProdutoController.java
@@ -0,0 +1,42 @@
+package com.github.DiegoCasemiroFS.vendas.controller;
+
+import com.github.DiegoCasemiroFS.vendas.entity.Produto;
+import com.github.DiegoCasemiroFS.vendas.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/api/produto")
+public class ProdutoController{
+
+    @Autowired
+
+    private ProdutoService produtoService;
+
+    @GetMapping("/{id}")
+    public Produto findById(@PathVariable Long id){
+    return produtoService.findById(id);
+    }
+
+    @GetMapping
+    public List<Produto> ListAll(){
+        return produtoService.ListAll();
+    }
+
+    @PostMapping
+    public Produto include(@RequestBody Produto produto){
+        return produtoService.include(produto);
+    }
+
+    @PutMapping("/{id}")
+    public Produto update(@PathVariable Long id, @RequestBody Produto produto){
+        return produtoService.update(id, produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete (@PathVariable Long id){
+        produtoService.delete(id);
+    }
+}
---
+++ src/main/java/com/github/DiegoCasemiroFS/vendas/service/ProdutoService.java
@@ -0,0 +1,41 @@
+package com.github.DiegoCasemiroFS.vendas.service;
+
+import com.github.DiegoCasemiroFS.vendas.entity.Produto;
+import com.github.DiegoCasemiroFS.vendas.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto findById(Long id){
+        return produtoRepository.findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
+    }
+
+    public List<Produto> ListAll(){
+        return produtoRepository.findAll();
+    }
+
+    public Produto include(Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    public Produto update(Long id, Produto produto){
+        findById(id);
+        produto.setId(id);
+        return produtoRepository.save(produto);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        produtoRepository.deleteById(id);
+    }
+}
